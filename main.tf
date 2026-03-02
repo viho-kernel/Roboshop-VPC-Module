b4328f72298cceaf5474e4548a9e0561ec9470fb
viho-kernel <vihari.reddy1802@gmail.com>
2026-03-02 17:00:02 +0530

    latest commit

--- main.tf
+++ main.tf
@@ -170,3 +170,10 @@ resource "aws_route" "database" {
   destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.nat.id
 }
+
+
+resource "aws_route_table_association" "public" {
+count = length(var.public_subnet_cidrs)
+subnet_id = aws_subnet.public[count.index].id 
+route_table_id = aws_route_table.public.id
+}
