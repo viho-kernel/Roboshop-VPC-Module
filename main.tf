9eb877f0c1f0b335eb64ad3a9346fb8f7ba41a20
viho-kernel <vihari.reddy1802@gmail.com>
2026-03-02 14:22:35 +0530

    latest commit

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
-  availability_zone = local.az_names
+  availability_zone = local.az_names[count.index]
 
   tags = merge (
     local.common_tags,
